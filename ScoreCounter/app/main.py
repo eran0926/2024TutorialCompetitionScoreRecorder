9201bee39e4fd6074b16afb6e4eda16b5f9321b2
Eran Hsu <eran0926@gmail.com>
2024-08-12 21:44:32 +0800

    add login done sync slect use dict storge user data

--- ScoreCounter/app/main.py
+++ ScoreCounter/app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO, ConnectionRefusedError
+from flask_socketio import SocketIO, join_room, leave_room, ConnectionRefusedError
 from flask_login import LoginManager, UserMixin, login_user, current_user, login_required, logout_user
 
 app = Flask(__name__)
@@ -9,22 +9,49 @@ socketio = SocketIO(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-users = {'a@a.a': {'password': 'a'}}  
+users = {
+    'admin': {
+        'password': 'admin',
+        'role': '0',
+        'alliance': 'admin'
+        },
+    'r1': {
+        'password': '1',
+        'role': '1',
+        'alliance': 'red'
+        },
+    'r2': {
+        'password': '1',
+        'role': '1',
+        'alliance': 'red'
+        },
+    'b1': {
+        'password': '1',
+        'role': '1',
+        'alliance': 'blue'
+        },
+    'b2': {
+        'password': '1',
+        'role': '1',
+        'alliance': 'blue'
+        }
+    }  
 
 class User(UserMixin):
     pass
 
 @login_manager.user_loader  
-def user_loader(email):  
+def user_loader(account):  
     """  
  設置二： 透過這邊的設置讓flask_login可以隨時取到目前的使用者id   
  :param email:官網此例將email當id使用，賦值給予user.id    
  """   
-    if email not in users:  
-        return  
   
     user = User()  
-    user.id = email  
+    user.id = account
+    user.role = users[account]['role']
+    user.alliance = users[account]['alliance']
+
     return user  
 
 @login_manager.unauthorized_handler
@@ -39,26 +66,25 @@ def login():
  官網git很給力的寫了一個login的頁面，在GET的時候回傳渲染     
  """   
     if request.method == 'GET':  
-           return '''
-     <form action='login' method='POST'>
-     <input type='text' name='email' id='email' placeholder='email'/>
-     <input type='password' name='password' id='password' placeholder='password'/>
-     <input type='submit' name='submit'/>
-     </form>
-                  '''
+           return render_template('login.html')
 
-    email = request.form['email']  
-    if request.form['password'] == users.get(email, {"password":""})['password']:  
+    account = request.form['account']
+    password = request.form['password']
+
+    if account not in users:  
+        return render_template('login.html', wrong_account=True)
+
+    if password == users[account]['password']:  
         #  實作User類別  
         user = User()  
         #  設置id就是email  
-        user.id = email  
+        user.id = account  
         #  這邊，透過login_user來記錄user_id，如下了解程式碼的login_user說明。  
         login_user(user)  
         #  登入成功，轉址  
         return redirect(url_for('index'))  
     
-    return 'Bad login' 
+    return render_template('login.html', wrong_password=True)
 
 @app.route('/logout')  
 def logout():  
@@ -73,36 +99,56 @@ def index():
     return redirect(url_for("counter"))
 
 @app.route('/counter')
-# @login_required
+@login_required
 def counter():
     return render_template("counter.html")
 
+@app.route('/scoreboard')
+def scoreboard():
+    return render_template("scoreboard.html")
+
+@app.route('/control')
+def control():
+    if int(current_user.role) > 0:
+        return "", 403
+    return render_template("control.html")
+
 @app.route('/test')
 def test():
-    return render_template("test.html")
+    return render_template("login copy.html")
+    # return render_template("test2.html")
+@app.route('/test2')
+def test2():
+    return render_template("test2.html")
 
 
 @socketio.on('connect')
 def connect():
-    print("\n",request)
-    print("\n",request.sid)
-    print("\n",request.args)
     if not current_user.is_authenticated:
         raise ConnectionRefusedError('unauthorized!')
     
     print("connected")
+    join_room(current_user.alliance)
     socketio.emit('sync_game_status', {
     "matchLevel":"practice",
     "matchNumber": 555,
     "matchStatus": "started",
-    "alliance": "blue",
+    "alliance": current_user.alliance,
     "team1": "435t-2",
     "team2": 12
 })
 
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data)
+@socketio.on('update_score')
+def update_score(data):
+    socketio.emit('update_score', data, to=current_user.alliance)
+    # print('received message: ' + data)
+    print("send to alliance: ", current_user.alliance)
+
+@socketio.on('update_selection')
+def update_selection(data):
+    socketio.emit('update_selection', data, to=current_user.alliance)
+    print("send to alliance: ", current_user.alliance)
+    print(data)
 
 if __name__ == '__main__':
     app.debug = True
--- ScoreCounter/app/static/scripts/main.js
+++ ScoreCounter/app/static/scripts/main.js
@@ -5,62 +5,48 @@ var initialX = null;
 var xThreshold = 0.3;
 var slide = 0;
 var config = {
-  "decrease-leave": "manual",
+    "decrease-leave": "manual",
 };
 var gameStatus = "stop";
 
 function swipePage(increment) {
-  previousSlide = slide;
-  slides = document.getElementById("main-holder").children;
-  if (slide + increment < slides.length && slide + increment >= 0) {
-    slides[slide].style.display = "none";
-    slide += increment;
-    window.scrollTo(0, 0);
-    slides[slide].style.display = "table";
-    // document.getElementById('data').innerHTML = "";
-    // document.getElementById('copyButton').setAttribute('value', 'Copy Data');
-  }
+    previousSlide = slide;
+    slides = document.getElementById("main-holder").children;
+    if (slide + increment < slides.length && slide + increment >= 0) {
+        slides[slide].style.display = "none";
+        slide += increment;
+        window.scrollTo(0, 0);
+        slides[slide].style.display = "table";
+        // document.getElementById('data').innerHTML = "";
+        // document.getElementById('copyButton').setAttribute('value', 'Copy Data');
+    }
 }
 
 function startTouch(e) {
-  initialX = e.touches[0].screenX;
+    initialX = e.touches[0].screenX;
 }
 
 function moveTouch(e) {
-  if (initialX === null) {
-    return;
-  }
-
-  var currentX = e.changedTouches[0].screenX;
-  var diffX = initialX - currentX;
-
-  // sliding horizontally
-  if (diffX / screen.width > xThreshold) {
-    // swiped left
-    if (slide != 0) {
-      swipePage(1);
+    if (initialX === null) {
+        return;
     }
-  } else if (diffX / screen.width < -xThreshold) {
-    // swiped right
-    swipePage(-1);
-  }
-  initialX = null;
+
+    var currentX = e.changedTouches[0].screenX;
+    var diffX = initialX - currentX;
+
+    // sliding horizontally
+    if (diffX / screen.width > xThreshold) {
+        // swiped left
+        if (slide != 0) {
+            swipePage(1);
+        }
+    } else if (diffX / screen.width < -xThreshold) {
+        // swiped right
+        swipePage(-1);
+    }
+    initialX = null;
 }
 
-document.querySelectorAll(".select-btn-group").forEach(function (element) {
-  element.addEventListener("click", function (e) {
-    Array.from(element.children).forEach(function (element) {
-      if (element == e.target) {
-        element.classList.remove("btn-secondary");
-        element.classList.add("btn-info");
-        config[element.parentElement.id] = element.value;
-      } else {
-        element.classList.remove("btn-info");
-        element.classList.add("btn-secondary");
-      }
-    });
-  });
-});
 // document.getElementById("auto-leave-select").addEventListener("click", function (e) {
 //     console.log(e);
 //     console.log(" ddefef");
@@ -78,145 +64,216 @@ document.querySelectorAll(".select-btn-group").forEach(function (element) {
 //     });
 // });
 
+function update_score(elementId, score) {
+    const cooectionId = socket.id;
+    socket.emit("update_score", {
+        id: elementId,
+        score: score,
+        from: cooectionId,
+    });
+}
+
+function update_selection(elementId, choice) {
+    const cooectionId = socket.id;
+    socket.emit("update_selection", {
+        id: elementId,
+        choice: choice,
+        from: cooectionId,
+    });
+}
+
 function setEventListener() {
-  allStage = document.getElementById("main-holder").children;
-  for (let i = 1; i < allStage.length; i++) {
-    allStage[i].style.display = "none";
-  }
-  for (let i = 0; i < allStage.length; i++) {
-    allStage[i].querySelectorAll(".count-btn").forEach(function (element) {
-      element.addEventListener("click", function (e) {
-        console.log(this);
-        score_div = this.querySelector(".score-div");
-        if (
-          !this.parentElement.parentElement
-            .querySelector("#decrease-btn")
-            .classList.contains("active")
-        ) {
-          score_div.innerText = parseInt(score_div.innerText) + 1;
-        } else {
-          if (parseInt(score_div.innerText) > 0) {
-            score_div.innerText = parseInt(score_div.innerText) - 1;
-          }
-          if (config["decrease-leave"] == "auto") {
-            this.parentElement.parentElement
-              .querySelector("#decrease-btn")
-              .classList.toggle("active");
-            this.parentElement.parentElement.querySelector(
-              "#decrease-btn"
-            ).innerText = "Decrease";
-          }
-        }
-      });
+    allStage = document.getElementById("main-holder").children;
+    for (let i = 1; i < allStage.length; i++) {
+        allStage[i].style.display = "none";
+    }
+
+
+    document.querySelectorAll(".select-btn-group").forEach(function (btn_group) {
+        btn_group.addEventListener("click", function (e) {
+            Array.from(btn_group.children).forEach(function (child) {
+                if (child == e.target) {
+                    child.classList.remove("btn-secondary");
+                    child.classList.add("btn-info");
+                    btn_group.value = child.value;
+                    update_selection(btn_group.id, btn_group.value);
+                    // config[element.parentElement.id] = element.value;
+                } else {
+                    child.classList.remove("btn-info");
+                    child.classList.add("btn-secondary");
+                }
+            });
+        });
     });
-    allStage[i].querySelectorAll("#decrease-btn").forEach(function (element) {
-      console.log(element);
-      element.addEventListener("click", function (e) {
-        this.classList.toggle("active");
-        if (this.classList.contains("active")) {
-          this.innerText = "Increase";
-        } else {
-          this.innerText = "Decrease";
-        }
-      });
-    });
-  }
+
+    for (let i = 0; i < allStage.length; i++) {
+        allStage[i].querySelectorAll(".count-btn").forEach(function (element) {
+            element.addEventListener("click", function (e) {
+                console.log(this);
+                score_div = this.querySelector(".score-div");
+                if (
+                    !this.parentElement.parentElement
+                        .querySelector("#decrease-btn")
+                        .classList.contains("active")
+                ) {
+                    score_div.innerText = parseInt(score_div.innerText) + 1;
+                    update_score(this.id, score_div.innerText);
+                } else {
+                    if (parseInt(score_div.innerText) > 0) {
+                        score_div.innerText = parseInt(score_div.innerText) - 1;
+                        update_score(this.id, score_div.innerText);
+                    }
+                    if (config["decrease-leave"] == "auto") {
+                        this.parentElement.parentElement
+                            .querySelector("#decrease-btn")
+                            .classList.toggle("active");
+                        this.parentElement.parentElement.querySelector(
+                            "#decrease-btn"
+                        ).innerText = "Decrease";
+                    }
+                }
+            });
+        });
+
+
+        allStage[i].querySelectorAll("#decrease-btn").forEach(function (element) {
+            console.log(element);
+            element.addEventListener("click", function (e) {
+                this.classList.toggle("active");
+                if (this.classList.contains("active")) {
+                    this.innerText = "Increase";
+                } else {
+                    this.innerText = "Decrease";
+                }
+            });
+        });
+
+    }
+
 }
 
 function start() {
-  if (gameStatus == "started") {
-    document.getElementById("start-btn").disabled = false;
+    if (gameStatus == "started") {
+        document.getElementById("start-btn").disabled = false;
 
-    // swipePage(1);
-  }
+        // swipePage(1);
+    }
 }
 
 function setSocket() {
-  var socket = io.connect();
+    socket = io.connect();
 
-  socket.on("connect", () => {
-    console.log("connected");
-    Array.from(document.getElementsByClassName("connection-status")).forEach(
-      function (element) {
-        element.classList.remove("danger");
-        element.innerText = "Connected";
-        element.classList.add("success");
-      }
-    );
-  });
-  socket.on("disconnect", () => {
-    Array.from(document.getElementsByClassName("connection-status")).forEach(
-      function (element) {
-        element.classList.remove("success");
-        element.innerText = "Disconnected";
-        element.classList.add("danger");
-      }
-    );
-  });
+    socket.on("connect", () => {
+        console.log("connected");
+        Array.from(document.getElementsByClassName("connection-status")).forEach(
+            function (element) {
+                element.classList.remove("danger");
+                element.innerText = "Connected";
+                element.classList.add("success");
+            }
+        );
+    });
 
-  socket.on("sync_game_status", function (msg) {
-    gameStatus = msg.matchStatus;
-    console.log(gameStatus);
-    if (gameStatus != "started") {
-      console.log(gameStatus);
-      document.getElementById("start-btn").disabled = true;
-    } else {
-      document.getElementById("start-btn").disabled = false;
-    }
-    Array.from(document.getElementById("level-select").children).forEach(
-      function (element) {
-        if (element.value == msg.matchLevel) {
-          element.classList.remove("btn-secondary");
-          element.classList.add("btn-info");
+    socket.on("disconnect", () => {
+        Array.from(document.getElementsByClassName("connection-status")).forEach(
+            function (element) {
+                element.classList.remove("success");
+                element.innerText = "Disconnected";
+                element.classList.add("danger");
+            }
+        );
+    });
+
+    socket.on("sync_game_status", function (msg) {
+        gameStatus = msg.matchStatus;
+        console.log(gameStatus);
+        if (gameStatus != "started") {
+            console.log(gameStatus);
+            document.getElementById("start-btn").disabled = true;
         } else {
-          element.classList.remove("btn-info");
-          element.classList.add("btn-secondary");
+            document.getElementById("start-btn").disabled = false;
         }
-      }
-    );
+        Array.from(document.getElementById("level-select").children).forEach(
+            function (element) {
+                if (element.value == msg.matchLevel) {
+                    element.classList.remove("btn-secondary");
+                    element.classList.add("btn-info");
+                } else {
+                    element.classList.remove("btn-info");
+                    element.classList.add("btn-secondary");
+                }
+            }
+        );
 
-    document.getElementById("matchNumberInput").value = msg.matchNumber;
+        document.getElementById("matchNumberInput").value = msg.matchNumber;
 
-    Array.from(document.getElementById("alliance-select").children).forEach(
-      function (element) {
-        if (element.value == msg.alliance) {
-          element.classList.remove("btn-secondary");
-          element.classList.add("btn-info");
-        } else {
-          element.classList.remove("btn-info");
-          element.classList.add("btn-secondary");
+        Array.from(document.getElementById("alliance-select").children).forEach(
+            function (element) {
+                if (element.value == msg.alliance) {
+                    element.classList.remove("btn-secondary");
+                    element.classList.add("btn-info");
+                } else {
+                    element.classList.remove("btn-info");
+                    element.classList.add("btn-secondary");
+                }
+            }
+        );
+
+        Array.from(document.getElementsByClassName("tean-number1")).forEach(
+            function (element) {
+                element.innerText = msg.team1;
+            }
+        );
+        Array.from(document.getElementsByClassName("tean-number2")).forEach(
+            function (element) {
+                element.innerText = msg.team2;
+            }
+        );
+    });
+
+    socket.on("gaem_start", function (msg) {
+        gameStatus = "started";
+        swipePage(1);
+    });
+    socket.on("game_stop", function (msg) {
+        gameStatus = "finished";
+        window.location.reload();
+    });
+
+    socket.on("update_score", function (msg) {
+        console.log(msg);
+        if (msg.from == socket.id) {
+            return;
         }
-      }
-    );
+        document.getElementById(msg.id).querySelector(".score-div").innerText =
+            msg.score;
+    });
 
-    Array.from(document.getElementsByClassName("tean-number1")).forEach(
-      function (element) {
-        element.innerText = msg.team1;
-      }
-    );
-    Array.from(document.getElementsByClassName("tean-number2")).forEach(
-      function (element) {
-        element.innerText = msg.team2;
-      }
-    );
-  });
-
-  socket.on("gaem_start", function (msg) {
-    gameStatus = "started";
-    swipePage(1);
-  });
-  socket.on("game_stop", function (msg) {
-    gameStatus = "finished";
-    window.location.reload();
-  });
+    socket.on("update_selection", function (msg) {
+        console.log(msg);
+        if (msg.from == socket.id) {
+            return;
+        }
+        Array.from(document.getElementById(msg.id).children).forEach(function (
+            element
+        ) {
+            if (element.value == msg.choice) {
+                element.classList.remove("btn-secondary");
+                element.classList.add("btn-info");
+            } else {
+                element.classList.remove("btn-info");
+                element.classList.add("btn-secondary");
+            }
+        });
+    });
 }
 
-function syncGameStatus() {}
+// function syncGameStatus(msg) { }
 
 function init() {
-  console.log("init");
-  setEventListener();
-  setSocket();
+    console.log("init");
+    setEventListener();
+    setSocket();
 }
 
 window.onload = init();
